27d4ae465b7ea74b4eeefe6c96ee5cefe0ef640d
DmitriyBystranov <dbystranov@mail.ru>
2017-05-21 22:44:49 +0400

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,17 @@
+/**
+	* Calculatore
+*/
 package ru.job4j;
+/**
+	*Class Calculate решение задачи части 001 урок 1.
+	*@author Bystranov
+	*@since 21.05.2017
+*/
 public class Calculate {
+	/**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
