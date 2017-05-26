8eab4c68f80c31d972d229f6859448df8ef37dbb
DmitriyBystranov <dbystranov@mail.ru>
2017-05-26 16:42:43 +0400

    4122

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -18,11 +18,10 @@ public class CalculateTest {
 		Test add.
 	*/
 	@Test
-
 	public void whenAddOneToOneThenTwo() {
 		ByteArrayOutputStream out  = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(out));
 		Calculate.main(null);
-		assertThat(out.toString(), is(String.format("Hello world!", System.getProperty("line.separator"))));
+		assertThat(out.toString(), is(String.format("Hello world!%s", System.getProperty("line.separator"))));
 	}
 }
