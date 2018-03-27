08ab30144eb3bb0dfa1ab34dfbbffceefa2270d6
DmitriyBystranov <dbystranov@mail.ru>
2018-03-28 02:25:20 +0400

    PhoneDictionary[#41598]1

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneDictionary {
-    private List<Person> person = new ArrayList<Person>();
+    private List<Person> persons = new ArrayList<Person>();
+
 
     public void add(Person person) {
-        this.person.add(person);
+        this.persons.add(person);
     }
 
     /**
@@ -16,7 +17,17 @@ public class PhoneDictionary {
      * @return Список подошедших пользователей.
      */
     public List<Person> find(String key) {
+        String allInformationOfPerson = null;
+        boolean agreement = false;
+
         List<Person> result = new ArrayList<>();
+        for(Person person : persons) {
+            allInformationOfPerson = person.getName() + person.getSurname() + person.getPhone() + person.getAddress();
+            agreement = allInformationOfPerson.contains(key);
+            if(agreement) {
+                result.add(person);
+            }
+        }
         return result;
     }
 }
